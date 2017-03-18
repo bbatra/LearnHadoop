364225f2f132cf0d36f7c656b5d72b453a4c3f32
bharat batra <bbatra@ucsd.edu>
2017-03-17 19:03:15 -0700

    Added readme, and comments

--- runtasks.sh
+++ runtasks.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# remove any existing network with this name
 sudo docker network rm hadoop
 
+# create a network for our nodes to interact
 sudo docker network create --driver=bridge hadoop
 
-
+# remove the existing image
 docker rm -f learn-hadoop
 
+# build the image using the Dockerfile present in this directory
 docker build -t learn-hadoop .
 
-# start hadoop master container
+# remove any previously running master container
 sudo docker rm -f hadoop-master &> /dev/null
-echo "start hadoop-master container..."
+
+# start master cintainer, exposing required ports and using the created network
 sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
@@ -21,19 +25,24 @@ sudo docker run -itd \
                 learn-hadoop &> /dev/null
 
 
-# start hadoop slave container
+
+echo "started hadoop-master container"
 i=1
+
+#we are going to start 4 client containers, removing any preexisting ones before running new ones
 for i in {1..4}
     do
-        sudo docker rm -f hadoop-slave$i &> /dev/null
-        echo "start hadoop-slave$i container..."
+        sudo docker rm -f hadoop-client$i &> /dev/null
+        echo "started hadoop-client$i container"
         sudo docker run -itd \
                         --net=hadoop \
-                        --name hadoop-slave$i \
-                        --hostname hadoop-slave$i \
+                        --name hadoop-client$i \
+                        --hostname hadoop-client$i \
                         learn-hadoop &> /dev/null
         i=$(( $i + 1 ))
     done
 
-# get into hadoop master container
+echo "Task 1 complete!!! Hadoop Cluster Started"
+
+# Execute the task script inside master container to run the remaining 2 tasks
 sudo docker exec -it hadoop-master bash /tasks.sh
--- tasks.sh
+++ tasks.sh
@@ -1,52 +1,62 @@
 #!/bin/bash
 
+# run the bootsrap script provided in the hadoop conainer to start Hadoop
 /bin/bash /etc/bootstrap.sh
 
-
+# move into the hadoop directory
 cd /usr/local/hadoop
 
-
-
+# bring the files from local file system into HDFS
 bin/hadoop fs -moveFromLocal /io/ io
 bin/hadoop fs -rmdir ./io/output-bc
+bin/hadoop fs -rmdir ./io/output-wc
 
+# environment variables to be used for java compilation in hadoop
 export JAVA_HOME=/usr/java/default
 
 export PATH=${JAVA_HOME}/bin:${PATH}
 
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 
-#bin/hadoop com.sun.tools.javac.Main WordCount.java
-#
-#jar cf wc.jar WordCount*.class
-#
+# compile the jave files in hadoop
+bin/hadoop com.sun.tools.javac.Main WordCount.java
+
+# make a jar using the class files
+jar cf wc.jar WordCount*.class
+
+# compile the java files in hadoop
 bin/hadoop com.sun.tools.javac.Main BigramCount.java
 
+#make a jar using the class files
 jar cf bc.jar BigramCount*.class
-#
-#echo "Running Word Count Example"
-#
-## Execute the JAR
-#bin/hadoop jar wc.jar WordCount ./io/input ./io/output-wc
-#
-#echo "Results of Distributed Word Count "
-#
-#bin/hadoop fs -cat ./io/output-wc/part-r-00000
+
+echo "Running Word Count Example"
+
+# Execute the wordcount JAR
+bin/hadoop jar wc.jar WordCount ./io/input ./io/output-wc
+
+echo "Results of Distributed Word Count (Task 2) "
+
+# read output of wordcount from hdfs
+bin/hadoop fs -cat ./io/output-wc/part-r-00000
 
 echo "Running Bigram Count"
 
+# Execute the Bigram Count JAR
 bin/hadoop jar bc.jar BigramCount ./io/input ./io/output-bc
 
-echo "Results of Distributed Word Count "
+echo "Results of Distributed Bigram Count (Task 3 part 1) "
 
+# read output of BigramCount from hdfs
 bin/hadoop fs -cat ./io/output-bc/part-r-00000
 
+# copy output of BigramCount to local filesystem for processing
 bin/hadoop fs -get ./io/output-bc/part-r-00000 ./answer
 
-
+# calculates the most common Bigram etc.
 javac Result.java
 
-echo "Getting the result"
+echo "Getting the result (Task 3 part 2 & 3) "
 
 java Result
 
